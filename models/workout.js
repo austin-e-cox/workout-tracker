b0aa1e09f88f2624e88a664f61b491e243b26c7b
Austin Cox <austin.ed.cox@gmail.com>
2020-04-10 23:26:59 -0700

    added essential components of schema, schema currently untested.

---
+++ models/workout.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+
+const workoutSchema = new Schema({
+  date: {
+    type: Date,
+    default: date.now()
+  },
+  exercises: [
+    {
+      name: {
+      type: String,
+      required: true
+      },
+      type: {
+        type: String,
+        required: true
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      }
+    }
+  ]
+})
